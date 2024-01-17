e6b1bcc235d7f16f849d70e62605469717cbffad
Adrian <adrian.asling@gmail.com>
2024-01-17 16:19:44 +0100

    enum

--- src/se/yrgo/Main.java
+++ src/se/yrgo/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(LazyRootUser.getInstance());
         System.out.println(StaticFinalRootUser.INSTANCE);
+        System.out.println(EnumRootUser.INSTANCE);
     }
 }
